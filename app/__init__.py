dc69fd78c801926bdf60536c51c62b9a06d5cf4c
pandysudhan <pandysudhan@gmail.com>
2023-09-08 13:00:04 -0500

    change app to create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
-from flask import Flask
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask import Flask, request, current_app
 from config import Config
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
 
+
+
     if not app.debug and not app.testing:
-    # ...
 
         if app.config['LOG_TO_STDOUT']:
             stream_handler = logging.StreamHandler()
@@ -19,8 +21,8 @@ def create_app(config_class=Config):
         else:
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/irisflower.log',
-                                                maxBytes=10240, backupCount=10)
+            file_handler = RotatingFileHandler('logs/microblog.log',
+                                               maxBytes=10240, backupCount=10)
             file_handler.setFormatter(logging.Formatter(
                 '%(asctime)s %(levelname)s: %(message)s '
                 '[in %(pathname)s:%(lineno)d]'))
@@ -28,9 +30,8 @@ def create_app(config_class=Config):
             app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Irisflower startup')
-    
+        app.logger.info('Microblog startup')
+
     return app
 
 
-from app import routes
