d3505683fc09be66b56582849a677067d0ff3f1d
pandysudhan <pandysudhan@gmail.com>
2023-09-12 07:28:53 -0500

    changed the main branch to normal app

--- app/__init__.py
+++ app/__init__.py
@@ -4,33 +4,33 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from config import Config
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+
+app = Flask(__name__)
+app.config.from_object(config_class)
 
 
-    if not app.debug and not app.testing:
-    # ...
+if not app.debug and not app.testing:
+# ...
 
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/irisflower.log',
-                                                maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/irisflower.log',
+                                            maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('Irisflower startup')
-    
-    return app
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Irisflower startup')
+
+
 
 
 from app import routes
