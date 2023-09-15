aa3710339df2c477b3ba4a77555ebe99deaa36de
pandysudhan <pandysudhan@gmail.com>
2023-09-15 09:24:13 -0500

    formated the return

--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,15 @@ class iris_predict(Resource):
     def get(self, features):
         features = features.split(',')
         for i in range(len(features)):
+            print(features)
             features[i] = float(features[i])
 
         print(features)
-        return model_loader.find_type_iris(features)
+        return {'sepal_length':features[0],
+                'sepal_width':features[1],
+                'sepal_length':features[2],
+                'petal_width':features[3],
+            'flower type':model_loader.find_type_iris(features)}
     
 
 class home(Resource):
