43067f958565e85f1f6157544ae590f989b9f92a
pandysudhan <pandysudhan@gmail.com>
2023-12-04 15:10:15 -0600

    backend commit

--- app/ml_integration/model_loader.py
+++ app/ml_integration/model_loader.py
@@ -2,12 +2,10 @@ import joblib
 import numpy as np
 
 
-def find_type_iris(list_of_features: list) -> str:
+def classify_survived(list_of_features: list) -> str:
 
-    types_of_flower = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
-    model = joblib.load('app/ml_integration/ml_model_test.joblib')
+    model = joblib.load('ml_integration/classification_model.joblib')
     test_data = np.array(list_of_features).reshape(1, -1)
     prediction = model.predict(test_data)
-    return (types_of_flower[prediction[0]])
+    return ([prediction[0]])
 
-print(find_type_iris([4.7, 3.2, 1.2, 0.2]))
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,21 @@
 from app import app
 from flask_restful import Resource, Api
-from app.ml_integration import model_loader
+from ml_integration import model_loader
+
 api= Api(app)
 
-class iris_predict(Resource):
+class titanic_classify(Resource):
     def get(self, features):
         features = features.split(',')
-        for i in range(len(features)):
-            print(features)
-            features[i] = float(features[i])
 
         print(features)
-        return {'sepal_length':features[0],
-                'sepal_width':features[1],
-                'sepal_length':features[2],
-                'petal_width':features[3],
-            'flower type':model_loader.find_type_iris(features)}
+        return {
+            'Result':model_loader.classify_survived(features)}
     
 
 class home(Resource):
     def get(self):
-        return "HEllo world"
+        return "Hello world"
     
 api.add_resource(home, '/')
-api.add_resource(iris_predict, '/<string:features>')
+api.add_resource(titanic_classify, '/<string:features>')
